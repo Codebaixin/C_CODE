70c59e2f08d2b120464906ad9c7481e0f305b546
Codebaixin <1129673550@qq.com>
2021-04-30 22:43:49 +0800

    C语言学习

---
+++ test.4.29/test.4.29/test.c
@@ -0,0 +1,31 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int len = strlen("abcdef");
+//	char arr[] = { 'a','b','c','d','e','f' };//找到\0停止计数。
+//	int len2 = strlen(arr);
+//	printf("%d %d\n", len, len2);
+//	return 0;
+//}
+//strlen的返回值的类型是size_t即无符号整形
+
+//#include<stdio.h>
+//int main()
+//{
+//	char* p1 = "abc";
+//	char* p2 = "acd";
+//	int ret = strcmp(p1, p2);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr1[10] = "abc";
+//	char arr2[] = "pokefgh";
+//	strncpy(arr1, arr2, 4);
+//	return 0;
+//}
